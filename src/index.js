f8697251d386276e2fbed49e2b86acb4ffb8e642
Void321 <96970580+Void321@users.noreply.github.com>
2022-02-03 20:33:20 +0900

    Weather App Project

---
+++ src/index.js
@@ -0,0 +1,76 @@
+let now = new Date();
+let h3 = document.querySelector("h3");
+let h4 = document.querySelector("h4");
+
+let date = now.getDate();
+let year = now.getFullYear();
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let hour = now.getHours();
+
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let day = days[now.getDay()];
+
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+let month = months[now.getMonth()];
+
+h3.innerHTML = `${day}, ${month} ${date}, ${year}`;
+
+h4.innerHTML = `${hour}:${minutes}`;
+
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let city = response.data.name;
+  let currentCity = document.querySelector("h1");
+  currentCity.innerHTML = `${city}`;
+  let currentTemp = document.querySelector("#temperature");
+  currentTemp.innerHTML = `${temperature}`;
+  let humidity = response.data.main.humidity;
+  let currentHumid = document.querySelector("#humidity");
+  currentHumid.innerHTML = `${humidity}`;
+  let wind = Math.round(response.data.wind.speed);
+  let currentWind = document.querySelector("#wind");
+  currentWind.innerHTML = `${wind}`;
+  let description = response.data.weather[0].main;
+  let currentDescription = document.querySelector("#description");
+  currentDescription.innerHTML = `${description}`;
+}
+
+function search(event) {
+  event.preventDefault();
+  let h1 = document.querySelector("h1");
+  let searchInput = document.querySelector("#search-text-input");
+  h1.innerHTML = `${searchInput.value}`;
+  let apiKey = "b81e7138d4f18ecdce4149c89f6f0058";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = temperatureElement.innerHTML;
+  temperature = Number(temperature);
+  temperatureElement.innerHTML = Math.round((temperature * 9) / 5 + 32);
+}
+
+let linkFahrenheit = document.querySelector("#fahrenheit-link");
+linkFahrenheit.addEventListener("click", convertToFahrenheit);
